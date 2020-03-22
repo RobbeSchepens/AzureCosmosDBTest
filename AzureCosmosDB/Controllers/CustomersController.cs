d8d31277390443e8ed3ff7442d94efaf7798aa99
Robbe Schepens <robbe.schepens@spray.com>
2020-03-22 22:12:18 +0100

    Implement GET Customers.

--- AzureCosmosDB/Controllers/CustomersController.cs
+++ AzureCosmosDB/Controllers/CustomersController.cs
@@ -9,29 +9,29 @@ namespace AzureCosmosDB.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class InvoicesController : ControllerBase
+    public class CustomersController : ControllerBase
     {
-        // GET: api/Invoices
+        // GET: api/Customers
         [HttpGet]
-        public async Task<IEnumerable<Invoice>> Get()
+        public async Task<IEnumerable<Customer>> Get()
         {
-            return await DocumentDBRepository<Invoice>.GetItemsAsync();
+            return await DocumentDBRepository<Customer>.GetItemsAsync();
         }
 
-        // GET: api/Invoices/5
-        [HttpGet("{id}", Name = "GetInvoice")]
-        public async Task<Invoice> Get(string id)
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public async Task<Customer> Get(string id)
         {
-            return await DocumentDBRepository<Invoice>.GetItemAsync(id);
+            return await DocumentDBRepository<Customer>.GetItemAsync(id);
         }
 
-        // POST: api/Invoices
+        // POST: api/Customers
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Invoices/5
+        // PUT: api/Customers/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
